2ff1e038d645bbaa28948a3aa1e5fa4680184e47
LoNGER-2111 <96116356+LoNGER-2111@users.noreply.github.com>
2023-05-24 13:11:14 +0700

    done status completed

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -9,19 +9,37 @@ interface Props {
   setTodos(todos: Todo[]): void;
 }
 
-export default class SingleTodo extends Component<Props> {
+interface State {
+  isDone: boolean;
+}
+
+export default class SingleTodo extends Component<Props, State> {
+  state = {
+    isDone: this.props.todo.isDone,
+  };
+
   handleDelete(id: number): void {
     this.props.setTodos(this.props.todos.filter((todo) => todo.id !== id));
   }
 
   render() {
     const { todo, index } = this.props;
+    const { isDone } = this.state;
 
     return (
-      <div className="list-item">
-        <p className="list-item__content">
-          {index}. {todo.todo}
-        </p>
+      <div
+        className="list-item"
+        onClick={() => this.setState({ isDone: !isDone })}
+      >
+        {isDone ? (
+          <p className="list-item__content line-through text-gray-400">
+            {index}. {todo.todo}
+          </p>
+        ) : (
+          <p className="list-item__content">
+            {index}. {todo.todo}
+          </p>
+        )}
         <i
           className="fa-solid fa-trash"
           onClick={() => this.handleDelete(todo.id)}
